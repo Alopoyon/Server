1fb9b9b9763f69b6a36a8b0f0636b7c112cc2da3
AchMat <43774510+Alopoyon@users.noreply.github.com>
2024-08-18 02:36:39 +0930

    a rudimentary project file system api

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv() 
+PROJECT_DIRECTORY = Path(os.environ['PROJECT_PATH'])
+
+from project_management import ProjectManagement
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], # here
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+@app.get("/api/projects/")
+async def projects():
+    project = ProjectManagement(PROJECT_DIRECTORY)
+    contents = project.directory_contents()
+    # print(f"{contents=}")
+    return contents
---
+++ project_management.py
@@ -0,0 +1,29 @@
+import pathlib
+from pathlib import Path
+
+class ProjectManagement:
+    def __init__(self,directory: pathlib.WindowsPath):
+        self._directory_contents = directory
+
+    def _return_directory_names(self):
+        __project_directories = []
+        for directory in self._directory_contents.iterdir():
+            if directory.is_dir():
+                __project_directories.append(directory.name)
+
+        return __project_directories
+
+    def _return_file_names(self):
+        __project_files = []
+        for file in self._directory_contents.iterdir():
+            if file.is_file():
+                __project_files.append(file.name)
+
+        return __project_files
+
+    def directory_contents(self):
+        _directory_contents = {
+            "directories": self._return_directory_names(),
+            "files": self._return_file_names()
+        }
+        return _directory_contents
