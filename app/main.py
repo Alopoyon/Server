29d4d4defc21166aa73575a4d9ce12a8128f8610
AchMat <43774510+Alopoyon@users.noreply.github.com>
2024-08-25 20:11:27 +0930

    working on tests, was getting import error, better to use app.main instead of ..app.main

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, FileResponse
 
 # CUSTOM PACKAGES/SCRIPTS
-from project_management import ProjectManagement
+from .project_management import ProjectManagement
 
 # UTILITY
 from pathlib import Path, PurePath
@@ -18,54 +18,51 @@ load_dotenv()
 PROJECT_DIRECTORY = Path(os.environ['PROJECT_PATH'])
 favicon_path = './favicon.ico' 
 
-def create_app():
-    app = FastAPI()
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://localhost:5173"],  # here
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # here
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    @app.get("/")
-    async def root():
-        return {"message": "Hello World!"}
+@app.get("/")
+async def root():
+    return {"msg": "Hello World!"}
 
-    @app.get('/favicon.ico', include_in_schema=False)
-    async def favicon():
-        print(os.getcwd())
-        return FileResponse(favicon_path)
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    print(os.getcwd())
+    return FileResponse(favicon_path)
 
-    @app.get("/api/projects")
-    async def projects():
-        _project = ProjectManagement(PROJECT_DIRECTORY)
+@app.get("/api/projects")
+async def projects():
+    _project = ProjectManagement(PROJECT_DIRECTORY)
+    _contents = _project.directory_contents()
+    # print(f"N{contents=}")
+    return _contents
+
+# @app.api_route("/api/projects/{path_name:path}", methods=["GET"])
+# async def catch_all_project_sub_calls(request:Request, path_name:str ):
+#     return {"request_method": request.method, "path_name": path_name}
+
+@app.get("/api/projects/{project_name:path}")
+async def project_contents(project_name: str = ""):
+    if project_name == "":
+        return JSONResponse(status_code=404, content={"message":"Invalid path!"})
+    # print(f"{project_name=}")
+    try:
+        _project = ProjectManagement(PROJECT_DIRECTORY.joinpath(project_name))
+        print("Current path: ",_project)
         _contents = _project.directory_contents()
-        # print(f"N{contents=}")
+        # print(f"{_contents=}")
         return _contents
-
-    # @app.api_route("/api/projects/{path_name:path}", methods=["GET"])
-    # async def catch_all_project_sub_calls(request:Request, path_name:str ):
-    #     return {"request_method": request.method, "path_name": path_name}
+    except:
+        return JSONResponse(status_code=404, content={"message":"Item does not exist"})
 
 
-    @app.get("/api/projects/{project_name:path}")
-    async def project_contents(project_name: str = ""):
-        if project_name == "":
-            return JSONResponse(status_code=404, content={"message":"Invalid path!"})
-        # print(f"{project_name=}")
-
-        try:
-            _project = ProjectManagement(PROJECT_DIRECTORY.joinpath(project_name))
-            print("Current path: ",_project)
-            _contents = _project.directory_contents()
-            # print(f"{_contents=}")
-            return _contents
-        except:
-            return JSONResponse(status_code=404, content={"message":"Item does not exist"})
-
-    return app
  
 if __name__ == "__main__":
-    uvicorn.run(app="main:create_app",host="127.0.0.1",port=8000,reload=True)
+    uvicorn.run(app="main:app",host="127.0.0.1",port=8000,reload=True)
